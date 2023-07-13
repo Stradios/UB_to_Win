2e079f5a8b7ee90ad5260c322636c09f29b419ce
Stradios <theodor.veres.85@gmail.com>
2023-07-13 17:14:02 +0200

    0.2.6

--- install-me.sh
+++ install-me.sh
@@ -197,8 +197,25 @@ print_large_text "${GREEN}Installing gnome-shell-Extension...${NC}"
 print_large_text "${RED}You need to reboot for this to apply!..${NC}"
 countdown
 cd ~/Downloads/UB_to_Win
-chmod +x gnome-shell-copy.sh
-sudo ./gnome-shell-copy.sh
+# Define the source and destination paths
+source_dir="gnome-shell"
+destination_dir="$HOME/.local/share"
+
+# Check if the source directory exists
+if [ ! -d "$source_dir" ]; then
+    echo "Source directory '$source_dir' does not exist."
+    exit 1
+fi
+
+# Create the destination directory if it doesn't exist
+if [ ! -d "$destination_dir" ]; then
+    mkdir -p "$destination_dir"
+fi
+
+# Copy the source directory to the destination
+cp -r "$source_dir" "$destination_dir"
+
+echo "Directory '$source_dir' copied to '$destination_dir'."
 
 clear
 # Install Extension Manager via Flatpak
